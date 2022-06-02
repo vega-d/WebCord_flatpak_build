d3a7494fa86715ee8f8ab479f012b32126f883f4
axtlos <axtlos@tar.black>
2022-06-02 17:32:10 +0200

    that didn't work

--- deps.sh
+++ deps.sh
@@ -8,4 +8,3 @@ elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
 
 sudo npm install --global electron-builder
-sudo chown -R 1001:121 "/github/home/.npm"
