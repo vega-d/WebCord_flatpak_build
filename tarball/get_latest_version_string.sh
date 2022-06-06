2b14c9d46ffd00192687b52678a37bf7c89d5d4b
vega-d <vegad0100110@gmail.com>
2022-06-06 15:20:02 +0200

    A single }. I am serious.

--- tarball/get_latest_version_string.sh
+++ tarball/get_latest_version_string.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-wget https://github.com/SpacingBat3/WebCord/releases/latest
+wget https://github.com/SpacingBat3/WebCord/releases/latest -q
 LATEST_RELEASE="$(cat latest | grep tar.gz | grep href | awk 'BEGIN { FS = "\"" }; { print $2 }')";
 echo "$LATEST_RELEASE" | grep -oP "^/SpacingBat3/WebCord/archive/refs/tags/\K.*" | sed -e s/.tar.gz$//
